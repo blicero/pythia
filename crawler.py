9dfe6bc28b5d86d43886901f56c93d502d5053f2
Benjamin Walkenhorst <krylon@posteo.de>
2024-02-22 22:44:24 +0100

    Add module crawler

---
+++ crawler.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-02-22 22:34:11 krylon>
+#
+# /data/code/python/pythia/crawler.py
+# created on 22. 02. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Wetterfrosch weather app. It is distributed
+# under the terms of the GNU General Public License 3. See the file
+# LICENSE for details or find a copy online at
+# https://www.gnu.org/licenses/agpl-3.0
+
+"""
+pythia.crawler
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+import logging
+from queue import SimpleQueue
+from threading import Lock, Thread
+
+from pythia import common, database
+
+
+class Crawler:
+    """Crawler traverses directory trees and inspects files."""
+
+    __slots__ = [
+        "log",
+        "db",
+        "folders"
+        "fileq",
+        "lock",
+        "active",
+        "workers",
+    ]
+
+    log: logging.Logger
+    db: database.Database
+    folders: list[str]
+    fileq: SimpleQueue
+    lock: Lock
+    active: bool
+    workers: list[Thread]
+
+    def __init__(self, *folders: str) -> None:
+        self.log = common.get_logger("crawler")
+        self.db = database.Database()
+        self.folders = folders
+        self.fileq = SimpleQueue()
+        self.lock = Lock()
+        self.active = False
+        self.workers = []
+
+    def is_active(self) -> bool:
+        with self.lock:
+            return self.active
+
+    def stop(self) -> None:
+        with self.lock:
+            self.active = False
+
+    def traverse(self) -> None:
+        """Start walking the directory tree(s)."""
+        with self.lock:
+            self.active = True
+            for tree in self.folders:
+                worker: Thread = Thread(target=self.__worker, args=(tree, ))
+                worker.start()
+                self.workers.append(worker)
+
+    def __worker(self, tree: str) -> None:
+        self.log.debug("Process folder %s", tree)
+        db = database.Database()
+        
+        for folder, subfolders, files in os.walk(tree):
+            pass
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
