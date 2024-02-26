2c8ea3d1d133112a459d5e4bfc0e48d3521cceaa
Benjamin Walkenhorst <krylon@posteo.de>
2024-02-26 20:07:34 +0100

    Add Testcase for data.File to test suffix extraction

---
+++ test_file.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Time-stamp: <2024-02-26 19:38:47 krylon>
+#
+# /data/code/python/pythia/test_file.py
+# created on 26. 02. 2024
+# (c) 2024 Benjamin Walkenhorst
+#
+# This file is part of the Wetterfrosch weather app. It is distributed
+# under the terms of the GNU General Public License 3. See the file
+# LICENSE for details or find a copy online at
+# https://www.gnu.org/licenses/agpl-3.0
+
+"""
+pythia.test_file
+
+(c) 2024 Benjamin Walkenhorst
+"""
+
+import unittest
+
+from pythia.data import File
+
+
+class FileTest(unittest.TestCase):
+    """Tests for the File."""
+
+    def test_find_suffix(self) -> None:
+        test_cases = (
+            ("/data/Documents/report2022.pdf", "pdf"),
+            ("/home/somebody/image.jpg", "jpg"),
+            ("/data/Curriculum_Vitae.docx", "docx"),
+            ("/home/krylon/.emacs", "emacs"),
+            ("/home/krylon/abobo", ""),
+        )
+
+        for c in test_cases:
+            f = File(c[0], {})
+            suffix = f.suffix()
+            self.assertEqual(suffix, c[1])
+
+# Local Variables: #
+# python-indent: 4 #
+# End: #
